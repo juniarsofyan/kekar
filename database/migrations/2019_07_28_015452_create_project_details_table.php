e1fb6e75dc26bbc51a6750b176bf8811d40728b2
juniarsofyan <choerulsofyanmf@gmail.com>
2019-07-28 09:45:17 +0700

    Create table migrations - create migrations for tables - create blank models

---
+++ database/migrations/2019_07_28_015452_create_project_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('project_id');
+            $table->integer('component_id');
+            $table->integer('material_id');
+            $table->string('dimension');
+            $table->text('problem');
+            $table->text('solution');
+            $table->integer('total_hours');
+            $table->integer('qty');
+            $table->decimal('weight', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_details');
+    }
+}
