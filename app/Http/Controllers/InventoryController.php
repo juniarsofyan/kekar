29f8dd6bb9a772c19fe51f6e8c0065c5a78f7424
juniarsofyan <choerulsofyanmf@gmail.com>
2019-07-30 07:06:01 +0700

    Master layout & blank controllers

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Inventory;
+
+class InventoryController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/ProjectDetailController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ProjectDetail;
+
+class ProjectDetailController extends Controller
+{
+    //
+}
