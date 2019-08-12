150ecfa0dad19f34ec4c15038100b8f11caaecc3
juniarsofyan <choerulsofyanmf@gmail.com>
2019-08-12 10:16:12 +0700

    Add Seeder for Card Work Detail Privileges

---
+++ database/seeds/CardWorkDetailSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CardWorkDetailSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'Card Work View',
+            'Card Work Create',
+            'Card Work Update',
+            'Card Work Delete'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
